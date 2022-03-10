4859fffeaefd463b8f2789c50d68b70f337d2543
unknown <hunny.rahi55@gmail.com>
2022-03-10 16:51:19 +0530

    Added Some More Components #2

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,42 @@
 <template>
+	<vue-element-loading
+		:active="loading"
+		is-full-screen
+		spinner="bar-fade-scale"
+		text="Please Wait.."
+		:size="70"
+		background-color="white"
+	/>
 	<router-view />
 </template>
 
 <script>
-	export default {};
+	import VueElementLoading from "vue-element-loading";
+	export default {
+		components: {
+			VueElementLoading,
+		},
+		data() {
+			return {
+				loading: true,
+			};
+		},
+		mounted() {
+			setTimeout(() => {
+				this.loading = false;
+			}, 4000);
+		},
+	};
 </script>
 
-<style></style>
+<style>
+	.fade-enter-active,
+	.fade-leave-active {
+		transition: opacity 1s ease;
+	}
+
+	.fade-enter-from,
+	.fade-leave-to {
+		opacity: 0;
+	}
+</style>
