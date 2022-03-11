ae539fb8b97d3436ea32e5593cec20268ee4d0f2
unknown <hunny.rahi55@gmail.com>
2022-03-11 18:44:39 +0530

    Removed VueScroll

--- src/main.js
+++ src/main.js
@@ -2,6 +2,5 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import VueElementLoading from "vue-element-loading";
-import VueSmoothScroll from 'vue3-smooth-scroll'
 
-createApp(App).use(router).component("VueElementLoading", VueElementLoading).use(VueSmoothScroll).mount('#app')
+createApp(App).use(router).component("VueElementLoading", VueElementLoading).mount('#app')
